49bd8cd016e15ab1fa50cd5418c896d019a9f5ae
lcmencia <luiscarlos@lcmencia.com>
2020-05-18 23:16:27 -0400

    create main.go and connect to db

--- main.go
+++ main.go
@@ -1,5 +1,16 @@
 package main
 
-func main() {
+import (
+	"log"
 
+	"github.com/lcmencia/tuitta/bd"
+	"github.com/lcmencia/tuitta/handlers"
+)
+
+func main() {
+	if bd.CheckConnection() == 0 {
+		log.Fatal("No connection to the database")
+		return
+	}
+	handlers.Handlers()
 }
